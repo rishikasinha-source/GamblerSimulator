c2e0acfcfbf7ba6675c368c0481e55442a2cf749
rishika7-hub <srishika970@gmail.com>
2020-12-03 10:02:27 +0530

    UC1

--- src/GamblerSimulator.java
+++ src/GamblerSimulator.java
@@ -4,6 +4,8 @@ public class GamblerSimulator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Gambler Simulator Problem");
+		int startStake= 100;
+		int bet=1;
 
 	}
 
