fef872c01fc5a41edb58d274b6fca53f823ac9ab
rishika7-hub <srishika970@gmail.com>
2020-12-03 09:57:06 +0530

    Welcome Message

---
+++ src/GamblerSimulator.java
@@ -0,0 +1,10 @@
+
+public class GamblerSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Gambler Simulator Problem");
+
+	}
+
+}
